f7e3d37d89e4a10286e48ce117d413f9d58bb4e7
DanAkrabi <danakrabi@gmail.com>
2025-03-26 18:28:30 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+function App() {
+  return <LoginPage />;
+}
+
+export default App;
